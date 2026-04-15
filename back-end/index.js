d97b9e9a78d36a813590f65769e859ece29e11e7
Tanush Mathur <devspark2501@gmail.com>
2026-04-15 13:54:56 +0530

    Add server side rendering comment in index.js

--- back-end/index.js
+++ back-end/index.js
@@ -5,6 +5,7 @@ app.use('/', function(req, res) {
     res.send('Node Backend Server'); // you should send a response
 });
 
+// server side rendering
 app.listen(4000, function() {
     console.log('Server Running on localhost:4000');
 });
