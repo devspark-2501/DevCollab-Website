1c19df45f071f677e2da53f27bfa0368b4283000
Tanush Mathur <devspark2501@gmail.com>
2026-04-19 13:39:08 +0530

    Complete Auth | debug

--- front-end/app/Profile/layout.js
+++ front-end/app/Profile/layout.js
@@ -1,9 +1,7 @@
-// import "./globals.css";
-
 export default function ProfileLayout({ children }) {
   return (
-    <html>
-      <body>{children}</body>
-    </html>
+    <div>
+      {children}
+    </div>
   );
 }
