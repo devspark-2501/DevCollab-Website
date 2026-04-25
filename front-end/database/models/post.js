03a533117698a7e0252b798a1dfcdb8f5d950f87
Tanush Mathur <devspark2501@gmail.com>
2026-04-26 01:07:42 +0530

    Updated and Debug the code!!

--- front-end/database/models/post.js
+++ front-end/database/models/post.js
@@ -1,31 +1,30 @@
-import mongoose, { models, Schema} from "mongoose";
+import mongoose, { models, Schema } from "mongoose";
 
-const PostSchema = new Schema(
-    {
-        userId: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-
-        userContent: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-
-        likes: {
-            type: Number,
-            default: 0,
-        },
-
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        }
-    }
-)
+const PostSchema = new Schema({
+  userEmail: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  userContent: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const Post = models.Post || mongoose.model("Post", PostSchema);
-
 export default Post;
