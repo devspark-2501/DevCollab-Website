12282890d5124085c308885cece1e7ae7cd33d7b
Tanush Mathur <devspark2501@gmail.com>
2026-04-26 16:21:11 +0530

    fix: remove reactCompiler

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // leave empty or add valid config only
 };
 
 export default nextConfig;
