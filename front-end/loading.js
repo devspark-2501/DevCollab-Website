f6f5a1bc520d3534bb2ab6ab011203966efa32ad
Tanush Mathur <devspark2501@gmail.com>
2026-05-07 14:10:48 +0530

    Add comment to Loading component

--- front-end/loading.js
+++ front-end/loading.js
@@ -2,6 +2,7 @@
 
 export default function Loading() {
   return (
+    // loading page!!
     <div className="min-h-screen flex items-center justify-center bg-[#0b0f1a] relative overflow-hidden">
 
       <div className="absolute w-[400px] h-[400px] bg-purple-600 opacity-20 blur-[120px] rounded-full top-[-100px] left-[-100px]" />
