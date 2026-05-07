5e6cef8730d43ecb8091c78d19837a1b769b25da
Tanush Mathur <devspark2501@gmail.com>
2026-05-07 15:34:56 +0530

    Updated the schema for Followers and Follows

--- front-end/database/models/user.js
+++ front-end/database/models/user.js
@@ -1,26 +1,31 @@
 import mongoose, { models, Schema } from "mongoose";
 
 const UserSchema = new Schema(
-    {
-        name: {
-            type: String, 
-            required: true
-        },
-
-        email: {
-            type: String, 
-            required: true
-        },
-
-        password: {
-            type: String,
-            required: true
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    // array of emails this user follows
+    following: [{ type: String }],
+    
+    // array of emails following this user
+    followers: [{ type: String }],
+  },
+  { timestamps: true }
 );
 
-// Prevent model overwrite upon initial compile
-const User = models.User || mongoose.model('User', UserSchema);
-
-export default User; 
+const User = models.User || mongoose.model("User", UserSchema);
+export default User;
