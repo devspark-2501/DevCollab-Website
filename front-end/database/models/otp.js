f9a3d16c65939228d6c96db4dea28086172fbec8
Tanush Mathur <devspark2501@gmail.com>
2026-06-01 06:06:32 +0530

    Add default comment to Otp model declaration

--- front-end/database/models/otp.js
+++ front-end/database/models/otp.js
@@ -7,5 +7,5 @@ const OtpSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now, expires: 300 }, // auto-delete after 5 min
 });
 
-const Otp = mongoose.models.Otp || mongoose.model("Otp", OtpSchema);
+const Otp = mongoose.models.Otp || mongoose.model("Otp", OtpSchema); // default
 export default Otp;
